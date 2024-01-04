0c9e0cc613acffc7d0eb6f5d8acbbb309f75dd85
PavelNik00 <gh9.111@yandex.ru>
2024-01-04 21:45:02 +0100

    Добавлен SingleImageViewController для показа изображения на весь экран.

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Pavel Nikipelov on 04.01.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
