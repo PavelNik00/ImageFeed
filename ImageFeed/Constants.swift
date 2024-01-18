f6bfe20a99c4036b0159baf446f2f54077c08c50
PavelNik00 <gh9.111@yandex.ru>
2024-01-18 19:11:47 +0100

    Создан файл Constants.swift и добавлены констаны с соответствующими данными для работы с Unsplash
    - AccessKey
    - SecretKey
    - RedirectURI
    - AccessDcope
    - DefaultBaseURL

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Pavel Nikipelov on 18.01.2024.
+//
+
+import Foundation
+
+let AccessKey = "9yYyoxsM08bps-2uFhkfBdTEZmwYp3YErtVt-N2wVxg"
+let SecretKey = "6cClc75p2bFawoFjs8J9fn8gqXii8RNIbkqSWQvo8Oc"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
