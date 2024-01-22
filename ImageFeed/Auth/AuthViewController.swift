bfcd5557d80595426f088937800dad53b8fd6572
PavelNik00 <gh9.111@yandex.ru>
2024-01-22 23:56:30 +0100

    Добавлен и сверстан AuthViewController в main. Добавлен swift файл соответственно.

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Pavel Nikipelov on 22.01.2024.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
