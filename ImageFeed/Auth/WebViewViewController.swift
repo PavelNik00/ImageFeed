f6b9968684cc3437d6dc346f66c69bfc2a28d601
PavelNik00 <gh9.111@yandex.ru>
2024-01-23 00:46:54 +0100

    Добавлен и сверстан WevViewViewController для показа WebView. (сам WebView и кнопка "назад")

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Pavel Nikipelov on 23.01.2024.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    
+    @IBOutlet private var webView: WKWebView!
+    
+    @IBAction func didTapButton(_ sender: Any) {
+    }
+}
