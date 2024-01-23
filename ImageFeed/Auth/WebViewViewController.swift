73bed7b009d20353071c7f5a6092fd0e44684f7c
PavelNik00 <gh9.111@yandex.ru>
2024-01-23 21:02:33 +0100

    Добавлен URLComponents во WebViewViewController для загрузки веб-контента.

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 import WebKit
 
+fileprivate let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+
 final class WebViewViewController: UIViewController {
     
     @IBOutlet private var webView: WKWebView!
     
     @IBAction func didTapButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: "AccessKey"),
+            URLQueryItem(name: "redirect_uri", value: RedirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: AccessScope)
+        ]
+        let url = urlComponents.url!
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
 }
