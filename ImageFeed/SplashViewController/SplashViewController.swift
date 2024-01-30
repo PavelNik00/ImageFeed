bc1c3afcf0f0d24a8de5de251e1662df9cca2a9e
PavelNik00 <gh9.111@yandex.ru>
2024-01-30 17:42:55 +0100

    Добавлен SplashViewController

---
+++ ImageFeed/SplashViewController/SplashViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SplashViewController.swift
+//  ImageFeed
+//
+//  Created by Pavel Nikipelov on 30.01.2024.
+//
+
+import Foundation
+
+final class SplashViewController {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+}
