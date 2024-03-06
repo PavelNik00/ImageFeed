abeffe37ba88d39a622b767ddf2b3198ad6e6ef6
PavelNik00 <gh9.111@yandex.ru>
2024-03-06 21:07:00 +0100

    Рефакторинг. Создание хелпера переноса кода из презентера для создания запроса к серверу и получение кода из ответа

---
+++ ImageFeed/Auth/AuthHelper.swift
@@ -0,0 +1,54 @@
+//
+//  AuthHelper.swift
+//  ImageFeed
+//
+//  Created by Pavel Nikipelov on 06.03.2024.
+//
+
+import Foundation
+
+protocol AuthHelperProtocol {
+    func authRequest() -> URLRequest?
+    func code(from url: URL) -> String?
+}
+
+final class AuthHelper: AuthHelperProtocol {
+    let configuration: AuthConfiguration
+    
+    init(configuration: AuthConfiguration = .standart) {
+        self.configuration = configuration
+    }
+    
+    func authRequest() -> URLRequest? {
+        guard let url = authURL() else { return nil }
+        
+        return URLRequest(url: url)
+    }
+    
+    func authURL() -> URL? {
+        guard var urlComponents = URLComponents(string: AuthConfiguration.standart.authURLString) else {
+            return nil
+        }
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        
+        return urlComponents.url
+    }
+    
+    func code(from url: URL) -> String? {
+           if let urlComponents = URLComponents(string: url.absoluteString),
+            urlComponents.path == "/oauth/authorize/native",
+            let items = urlComponents.queryItems,
+            let codeItem = items.first(where: { $0.name == "code" })
+        {
+            return codeItem.value
+        } else {
+            return nil
+        }
+    }
+    
+}
--- ImageFeed/Auth/WebViewPresenter.swift
+++ ImageFeed/Auth/WebViewPresenter.swift
@@ -19,32 +19,40 @@ public protocol WebViewPresenterProtocol {
 final class WebViewPresenter: WebViewPresenterProtocol {
     weak var view: WebViewViewControllerProtocol?
     
+    var authHelper: AuthHelperProtocol
+    
+    init(authHelper: AuthHelperProtocol) {
+        self.authHelper = authHelper
+    }
+    
 //    fileprivate let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
 
     func viewDidLoad() {
-        guard var urlComponents = URLComponents(string: AuthConfiguration.standart.authURLString)
-        else {
-            assertionFailure("Invalid authorization URL string: \(AuthConfiguration.standart.authURLString)")
+//        guard var urlComponents = URLComponents(string: AuthConfiguration.standart.authURLString)
+//        else {
+//            assertionFailure("Invalid authorization URL string: \(AuthConfiguration.standart.authURLString)")
+//            return
+//        }
+//
+//        urlComponents.queryItems = [
+//            URLQueryItem(name: "client_id", value: Constants.accessKey),
+//            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+//            URLQueryItem(name: "response_type", value: "code"),
+//            URLQueryItem(name: "scope", value: Constants.accessScope)
+//        ]
+//
+//        guard let url = urlComponents.url else {
+//            assertionFailure("Failed to construct authorization URLRequest with components: \(urlComponents)")
+//            return
+//        }
+
+        guard let request = authHelper.authRequest() else {
+            assertionFailure("Failed to construct authorization URLRequest")
             return
         }
-
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: Constants.accessKey),
-            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: Constants.accessScope)
-        ]
-
-        guard let url = urlComponents.url else {
-            assertionFailure("Failed to construct authorization URLRequest with components: \(urlComponents)")
-            return
-        }
-
-        let request = URLRequest(url: url)
-        
-        didUpdateProgressValue(0)
         
         view?.load(request: request)
+        didUpdateProgressValue(0)
     }
     
     func didUpdateProgressValue(_ newValue: Double) {
@@ -61,14 +69,15 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     
     // презентер анализирует URL и достает из него код, если он есть, реализация метода
     func code(from url: URL) -> String? {
-           if let urlComponents = URLComponents(string: url.absoluteString),
-            urlComponents.path == "/oauth/authorize/native",
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: { $0.name == "code" })
-        {
-            return codeItem.value
-        } else {
-            return nil
-        }
+        authHelper.code(from: url)
+//           if let urlComponents = URLComponents(string: url.absoluteString),
+//            urlComponents.path == "/oauth/authorize/native",
+//            let items = urlComponents.queryItems,
+//            let codeItem = items.first(where: { $0.name == "code" })
+//        {
+//            return codeItem.value
+//        } else {
+//            return nil
+//        }
     }
 }
