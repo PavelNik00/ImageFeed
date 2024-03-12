950337fb4aa1287d47a837c33ab6b1e30febd2ff
PavelNik00 <gh9.111@yandex.ru>
2024-03-12 16:17:53 +0100

    Unit-тест. Проверка необходимость скрытия прогресса. Добавлено 2 теста для проверки значения прогресса меньше единицы, и равной единице.

--- ImageFeedTests/ImageFeedTests.swift
+++ ImageFeedTests/ImageFeedTests.swift
@@ -12,6 +12,7 @@ import Foundation
 
 final class ImageFeedTests: XCTestCase {
 
+    // тест для проверки вызова viewDidLoad презентера и вьюконтроллера
     func testViewControllerCallsViewDidLoad() {
         // given
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -27,6 +28,7 @@ final class ImageFeedTests: XCTestCase {
         XCTAssertTrue(presenter.viewDidLoadCalled)
     }
     
+    // тест проверки вызова метода loadRequest вьюконтроллера после вызова presenter.viewDidLoad()
     func testPresenterCallsLoadRequest() {
         // given
         let viewController = WebViewViewControllerSpy()
@@ -43,11 +45,34 @@ final class ImageFeedTests: XCTestCase {
         XCTAssertTrue(viewController.loadRequestCalled)
     }
     
+    // тест для проверки значения прогресса равной меньше 1
+    func testProgressVisibleWhenLessThenOne() {
+        //given
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 0.6
+        
+        //when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        //then
+        XCTAssertFalse(shouldHideProgress)
+    }
+    
+    // тест для проверки значения прогресса равной 1
+    func testProgressHiddenWhenOne() {
+        //given
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 1.0
+        
+        //when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        //then
+        XCTAssertTrue(shouldHideProgress)
+    }
     override func setUpWithError() throws { }
 
     override func tearDownWithError() throws { }
-
-    func testExample() throws { }
-
-    func testPerformanceExample() throws { measure { } }
 }
